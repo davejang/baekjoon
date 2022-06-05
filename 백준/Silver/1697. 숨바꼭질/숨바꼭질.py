980b4387dc1a40fda1d0abe5d54ae24944647ed2
EVERGREEN <davejang98@naver.com>
2022-06-05 13:29:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 412 ms, Memory: 38544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -6,14 +6,6 @@ visited = [0]*(200001)
 dp = [-1,1,0]
 dx = [0,0,1]
 
-for i in range(n,100000):
-  near_node_list = []
-  near_node_list.append(2*i)
-  near_node_list.append(i+1)
-  if i != n:
-    near_node_list.append(i-1)
-  graph[i] = near_node_list
-
 queue = deque([])
 queue.append(n)
 while queue:
