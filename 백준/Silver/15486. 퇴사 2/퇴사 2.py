2884b874f67028a5b8733563d806fe1bee8df6dc
EVERGREEN <davejang98@naver.com>
2022-09-11 16:42:05 +0900

    [Silver I] Title: 퇴사 2, Time: 2644 ms, Memory: 250912 KB -BaekjoonHub

---
+++ 백준/Silver/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+array = []
+dp = [0]*(n+1)
+for i in range(n):
+  a, b = map(int,input().split())
+  array.append((a,b))
+for i in range(n-1,-1,-1):
+  if i + array[i][0] > n:
+    dp[i] = dp[i+1]
+  else:
+    dp[i] = max(dp[i+1],array[i][1]+dp[i+array[i][0]])
+print(dp[0])
