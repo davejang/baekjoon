f57a9f55f33f47fc4178a5e579f0c445a9d2c632
KyeongSeok Jang <davejang98@naver.com>
2023-04-13 13:22:58 +0900

    [level 2] Title: 모음 사전, Time: 1.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -5,8 +5,5 @@ def solution(word):
     for i in range(1,6):
         word_dict += list(''.join(p) for p in product(['A','E','I','O','U'],repeat=i))
     word_dict.sort()
-    for i in range(len(word_dict)):
-        if word_dict[i] == word:
-            answer = i + 1
-            break
-    return answer
+
+    return word_dict.index(word) + 1
