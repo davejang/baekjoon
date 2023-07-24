a28a1134de5ab4dc9a1f6b3d72e7e4f7be7b4edf
KyeongSeok Jang <davejang98@naver.com>
2023-07-24 23:19:30 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 548 ms, Memory: 40060 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,37 +1,44 @@
 from collections import deque
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[-1] * M for _ in range(N)]
-dx, dy = [0,0,-1,1], [-1,1,0,0]
 
-def bfs(i,j):
-    queue = deque()
-    queue.append((i,j))
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-    visited[i][j] = 0
+n, m = map(int,input().split())
+graph = []
+visited = [[-1 for col in range(m)] for row in range(n)]
 
-    while queue:
-        x, y = queue.popleft()
+for i in range(n):
+  graph.append(list(map(int,input().split())))
+
+def bfs(a,b):
+  queue = deque([])
+  queue.append((a,b))
+  visited[a][b] = 0
+  
+  while queue:
+    x, y = queue.popleft()
+    
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx < 0 or nx >= n or ny < 0 or ny >= m:
+        continue
+      if graph[nx][ny] == 0:
+        visited[nx][ny] = 0
+      elif visited[nx][ny] == -1:
+        visited[nx][ny] = visited[x][y] + 1
+        queue.append((nx,ny))
+
+for i in range(n):
+    for k in range(m):
+        if graph[i][k] == 2 and visited[i][k]== -1:
+            bfs(i,k)
+
+for i in range(n):
+  for j in range(m):
+    if graph[i][j] == 0:
+      print(0,end = ' ')
+    else:
+      print(visited[i][j],end = ' ')
+  print()
         
-        for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            
-            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == -1:
-                if graph[nx][ny] == 0: 
-                    visited[nx][ny] = 0
-                elif graph[nx][ny] == 1:
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.append((nx,ny))
-
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 2 and visited[i][j] == -1:
-            bfs(i,j)
-
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 0:
-            print(0, end=' ')
-        else:
-            print(visited[i][j], end=' ')
-    print()
