d8ae6af1d674124e599cf084ef0d7ddc1268ce08
KyeongSeok Jang <davejang98@naver.com>
2023-09-08 12:40:08 +0900

    [Silver I] Title: 점프, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[0 for _ in range(n)] for _ in range(n)]
+dp[0][0] = 1
+graph  = [list(map(int,input().rstrip().split())) for _ in range(n)]
+
+for i in range(n):
+  for j in range(n):
+
+    if i == n - 1 and j == n - 1:
+        print(dp[i][j])
+        break
+
+    cost = graph[i][j]
+    if i + cost < n:
+      dp[i+cost][j] += dp[i][j]
+
+    if j + cost < n:
+      dp[i][j+cost] += dp[i][j]
