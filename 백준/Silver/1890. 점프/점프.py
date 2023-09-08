88882904b67c2b17dc1d5b40437558b6815f042f
KyeongSeok Jang <davejang98@naver.com>
2023-09-08 12:43:02 +0900

    [Silver I] Title: 점프, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -10,13 +10,14 @@ graph  = [list(map(int,input().rstrip().split())) for _ in range(n)]
 for i in range(n):
   for j in range(n):
 
-    if i == n - 1 and j == n - 1:
-        print(dp[i][j])
-        break
-
+    if dp[i][j] == 0 or graph[i][j] == 0:
+      continue
+      
     cost = graph[i][j]
     if i + cost < n:
       dp[i+cost][j] += dp[i][j]
 
     if j + cost < n:
       dp[i][j+cost] += dp[i][j]
+
+print(dp[-1][-1])
