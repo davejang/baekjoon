7b18206e830398d29d83460676559e87592a1ed9
KyeongSeok Jang <davejang98@naver.com>
2023-09-08 16:11:13 +0900

    [Gold V] Title: 합분해, Time: 56 ms, Memory: 33300 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1,k+1):
+  dp[0][i] = 1
+for i in range(1,n+1):
+  dp[i][1] = 1
+
+for i in range(1,n+1):
+  for j in range(1,k+1):
+    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+print(dp[-1][-1] % 1000000000)
