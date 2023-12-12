3a380662518b2f796961d3b8399779aedf32b7a2
KyungSeok Jang <davejang98@naver.com>
2023-12-12 17:51:52 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 116 ms, Memory: 38324 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+arr = []
+stack = []
+
+n = int(input())
+result = 0
+result_count = [0 for _ in range(n)]
+
+for i in range(n):
+  arr.append(int(input()))
+
+for i in range(n):
+  while stack and arr[stack[-1]] <= arr[i]:
+    stack.pop()
+    result += len(stack)
+
+  stack.append(i)
+
+
+print(result + (len(stack) * (len(stack) - 1))//2)
